3d5802539f470db17b9d818d05f775f448eca296
sharipov <ruslan.b.sharipov@gmail.com>
2021-12-11 16:39:08 +0300

    добавление табов вьюпейджеру

--- app/src/main/java/ru/surfstudio/surf_team/f_main/MainScreen.kt
+++ app/src/main/java/ru/surfstudio/surf_team/f_main/MainScreen.kt
@@ -1,11 +1,11 @@
 package ru.surfstudio.surf_team.f_main
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
@@ -17,6 +17,9 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 import ru.surfstudio.surf_team.R
 import ru.surfstudio.surf_team.f_about.AboutScreen
 import ru.surfstudio.surf_team.f_team_members.TeamMembersScreen
@@ -25,13 +28,50 @@ import ru.surfstudio.surf_team.ui.theme.SurfteamTheme
 @ExperimentalPagerApi
 @Composable
 fun MainScreen() {
-    val viewModel = viewModel(modelClass = MainViewModel::class.java, factory = MainViewModel.Factory())
+    val viewModel =
+        viewModel(modelClass = MainViewModel::class.java, factory = MainViewModel.Factory())
     val state by viewModel.observeState().collectAsState(initial = MainState.Loading)
 
-    when(state){
+    when (state) {
         MainState.Error -> MainErrorState { viewModel.handleRetry() }
         MainState.Loading -> MainLoadingState()
-        is MainState.Success -> HorizontalPager(count = 2) {
+        is MainState.Success -> MainSuccessState(state)
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+private fun MainSuccessState(state: MainState) {
+    val pagerState = rememberPagerState()
+    val scope = rememberCoroutineScope()
+    Column {
+        TabRow(
+            // Our selected tab is our current page
+            selectedTabIndex = pagerState.currentPage,
+            // Override the indicator, using the provided pagerTabIndicatorOffset modifier
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
+            }
+        ) {
+            // Add tabs for all of our pages
+            MainTab.values().forEachIndexed { index: Int, tab: MainTab ->
+                Tab(
+                    text = { Text(stringResource(id = tab.titleRes)) },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                          scope.launch {
+                              pagerState.scrollToPage(index)
+                          }
+                    },
+                )
+            }
+        }
+        HorizontalPager(
+            count = MainTab.values().size,
+            state = pagerState
+        ) {
             if (it == 0) {
                 TeamMembersScreen(
                     members = (state as MainState.Success).teamUnits
