fe97ac7a3f38c4cc23f6f5e48a54095aa9db977d
sharipov <ruslan.b.sharipov@gmail.com>
2021-12-11 13:50:03 +0300

    верстка вкладки о приложении

---
+++ app/src/main/java/ru/surfstudio/surf_team/f_about/AboutScreen.kt
@@ -0,0 +1,67 @@
+package ru.surfstudio.surf_team.f_about
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.surfstudio.surf_team.R
+import ru.surfstudio.surf_team.ui.theme.SurfteamTheme
+
+@Composable
+fun AboutScreen() {
+    Column {
+        Column(modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_android),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(Color.LightGray), // TODO заменить на цвет из темы
+                modifier = Modifier
+                    .size(56.dp, 32.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = stringResource(id = R.string.about_subtitle),
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .padding(top = 16.dp),
+                textAlign = TextAlign.Center
+            )
+            TextButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(24.dp)
+            ) {
+                Text(text = stringResource(id = R.string.about_github))
+            }
+        }
+
+        Box(contentAlignment = Alignment.BottomCenter) {
+            TextButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) {
+                Text(text = stringResource(id = R.string.about_chat_btn))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AboutPreview() {
+    SurfteamTheme(darkTheme = true) {
+        AboutScreen()
+    }
+}
--- app/src/main/java/ru/surfstudio/surf_team/ui/theme/Theme.kt
+++ app/src/main/java/ru/surfstudio/surf_team/ui/theme/Theme.kt
@@ -1,22 +1,20 @@
 package ru.surfstudio.surf_team.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
-    primaryVariant = Bunker,
+    primaryVariant = Purple700,
     secondary = Teal200,
     background = Bunker
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
-    primaryVariant = Whisper,
+    primaryVariant = Purple700,
     secondary = Teal200,
     background = Whisper
 
